50c34260e549c4923c18d2e88fc80dc483aa5abc
nick <735937076@qq.com>
2020-03-11 23:15:49 +0800

    Q12 add

--- src/Question11/Solution.java
+++ src/Question11/Solution.java
@@ -16,7 +16,8 @@ import java.util.Map;
 public class Solution {
     public static void main(String[] args) {
         int[] height  = {1,8,6,2,5,4,8,3,7};
-        System.out.println(maxArea2(height));
+        System.out.println("maxArea == "+maxArea(height));
+        System.out.println("maxArea2 == "+maxArea2(height));
     }
 
     //暴力法
---
+++ src/Question12/Solution.java
@@ -0,0 +1,79 @@
+package Question12;
+
+/**
+ * 罗马数字包含以下七种字符： I， V， X， L，C，D 和 M。
+ *
+ * 字符          数值
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+ * 例如， 罗马数字 2 写做 II ，即为两个并列的 1。12 写做 XII ，即为 X + II 。 27 写做  XXVII, 即为 XX + V + II 。
+ *
+ * 通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，所表示的数等于大数 5 减小数 1 得到的数值 4 。同样地，数字 9 表示为 IX。这个特殊的规则只适用于以下六种情况：
+ *
+ * I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
+ * X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。 
+ * C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
+ * 给定一个整数，将其转为罗马数字。输入确保在 1 到 3999 的范围内。
+ *
+ * 示例 1:
+ *
+ * 输入: 3
+ * 输出: "III"
+ * 示例 2:
+ *
+ * 输入: 4
+ * 输出: "IV"
+ * 示例 3:
+ *
+ * 输入: 9
+ * 输出: "IX"
+ * 示例 4:
+ *
+ * 输入: 58
+ * 输出: "LVIII"
+ * 解释: L = 50, V = 5, III = 3.
+ * 示例 5:
+ *
+ * 输入: 1994
+ * 输出: "MCMXCIV"
+ * 解释: M = 1000, CM = 900, XC = 90, IV = 4.
+ * @Author: Nick
+ * @Date: 2020/3/11 22:30
+ */
+public class Solution {
+    public static void main(String[] args) {
+        int num = 2095;
+        System.out.println(intToRoman(num));
+    }
+
+    public static String intToRoman(int num) {
+        //EdgeCase:
+        if(num < 0 || num > 3999){
+            return "";
+        }
+
+        String[][] arr = {
+            {"","I","II","III","IV","V","VI","VII","VIII","IX"},
+            {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"},
+            {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"},
+            {"","M","MM","MMM","","","","","",""}
+        };
+
+        //千位
+        int a = num /1000;
+        //百位
+        int b = num / 100 % 10;
+        //十位
+        int c = num / 10 % 10;
+        //个位
+        int d = num % 10;
+
+        StringBuilder result = new StringBuilder();
+        return result.append(arr[3][a]).append(arr[2][b]).append(arr[1][c]).append(arr[0][d]).toString();
+    }
+}
