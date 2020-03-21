6e4371272d3e3dc7caae6375417d07932cbfd732
nick <735937076@qq.com>
2020-03-21 14:13:47 +0800

    Q19 add

---
+++ src/Question19/ListNode.java
@@ -0,0 +1,11 @@
+package Question19;
+
+/**
+ * @Author: WZL
+ * @Date: 2020/3/20 13:54
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
--- src/Question19/Solution.java
+++ src/Question19/Solution.java
@@ -5,4 +5,47 @@ package Question19;
  * @Date: 2020/3/13 16:08
  */
 public class Solution {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+
+        ListNode temp = head;
+        for(int i = 2 ; i < 6 ; i++){
+            temp.next = new ListNode(i);
+            temp = temp.next;
+        }
+
+        head = removeNthFromEnd(head,2);
+
+        while(head != null){
+            System.out.println(head.val);
+            head = head.next;
+        }
+    }
+
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode pre = new ListNode(0);
+        pre.next = head;
+        ListNode temp = head;
+        int length = 0;
+        while(temp != null){
+            temp = temp.next;
+            length ++;
+        }
+
+        temp = pre;
+        length = length - n;
+        if(length < 0){
+            return null;
+        }
+
+        while(length > 0){
+           temp = temp.next;
+           length --;
+        }
+        temp.next = temp.next.next;
+
+        return pre.next;
+    }
 }
+
+
