1d410abdbb0883adc3d0db8916bc1fe480cc2ddf
nick <735937076@qq.com>
2020-03-23 16:01:12 +0800

    Q21 add

---
+++ src/Question21/ListNode.java
@@ -0,0 +1,11 @@
+package Question21;
+
+/**
+ * @Author: WZL
+ * @Date: 2020/3/23 15:14
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
--- src/Question21/Solution.java
+++ src/Question21/Solution.java
@@ -5,4 +5,40 @@ package Question21;
  * @Date: 2020/3/13 16:08
  */
 public class Solution {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(3);
+        ListNode l2 = new ListNode(2);
+        l2.next = new ListNode(4);
+        l2.next.next = new ListNode(5);
+
+        printListNode(l1,"l1");
+        printListNode(l2,"l2");
+        l1 = mergeTwoLists(l1,l2);
+        printListNode(l1,"result");
+    }
+
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode temp = new ListNode(-1);
+        ListNode result = temp;
+        while(null != l1 || null != l2){
+            if(null == l2 || (null != l1 && l1.val <= l2.val)){
+                temp.next = new ListNode(l1.val);
+                l1 = l1.next;
+            }else if(null == l1 || (null != l2 && l1.val > l2.val)){
+                temp.next = new ListNode(l2.val);
+                l2 = l2.next;
+            }
+            temp = temp.next;
+        }
+        return result.next;
+    }
+
+    public static void printListNode(ListNode listNode,String msg){
+        while(listNode != null){
+            System.out.println(msg+"=="+listNode.val);
+            listNode = listNode.next;
+        }
+    }
 }
