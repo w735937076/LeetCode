89f3eaff6722ec9e8b79181044a2ac21cfeb4d38
nick <735937076@qq.com>
2020-03-24 21:19:38 +0800

    Q22 add

--- src/Question22/Solution.java
+++ src/Question22/Solution.java
@@ -1,8 +1,48 @@
 package Question22;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Author: WZL
  * @Date: 2020/3/13 16:08
  */
 public class Solution {
+    public static void main(String[] args) {
+        printList(generateParenthesis(4));
+    }
+
+    public static List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList<String>();
+        generate(list,"",0,0,n);
+        return list;
+    }
+
+    public static void generate(List<String> list,String result,int leftNum,int rightNum,int n){
+
+        if(leftNum > n || rightNum > n){
+            return;
+        }
+
+        if(leftNum == n &&  rightNum == n){
+            list.add(result);
+        }
+
+        if(leftNum < n){
+            generate(list,result+"(",leftNum + 1,rightNum,n);
+        }
+
+        if(rightNum <  leftNum){
+            generate(list,result+")",leftNum,rightNum + 1,n);
+        }
+    }
+
+    public static void printList(List<String> list){
+        if(null != list && list.size() > 0){
+            int index = 1;
+            for(String str : list){
+                System.out.println("第"+ (index++) +"种组合："+str);
+            }
+        }
+    }
 }
